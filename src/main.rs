951144f5ac761c904079f51be2a0d218cacee071
Dominik Winecki <dominikwinecki@gmail.com>
2021-04-11 21:39:57 -0400

    Working simple equality filter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sqlparser::ast::*;
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
+use std::cmp::PartialEq;
 use std::collections::HashMap;
 
 use json::{object, JsonValue};
@@ -20,6 +21,11 @@ struct MetaTableDef {
     schema: MetaSchema,
 }
 
+#[derive(Debug, Clone)]
+struct LocalSchema {
+    columns: Vec<ColRef>,
+}
+
 #[derive(Debug, Clone, Copy)]
 enum MetaType {
     LONG,
@@ -35,6 +41,7 @@ struct MetaSchema {
 enum Op {
     ScanOp(Box<OpScan>),
     JoinOp(Box<OpJoin>),
+    FilterOp(Box<OpFilter>),
 }
 
 #[derive(Debug)]
@@ -44,6 +51,7 @@ struct OpScan {
     filetype: String,
     tab_name: String,
     schema: Vec<MetaType>,
+    ls: LocalSchema,
 }
 
 #[derive(Debug)]
@@ -53,9 +61,20 @@ struct OpJoin {
     build_join_attribute: u32,
     probe: Op,
     probe_join_attribute: u32,
+    ls: LocalSchema,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
+struct OpFilter {
+    r#type: String,
+    input: Op,
+    op: String,
+    field: u32,
+    value: String,
+    ls: LocalSchema,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 struct ColRef {
     table: String,
     column: String,
@@ -66,6 +85,52 @@ enum Selection {
     Identity(ColRef),
     And(Vec<Selection>),
     Eq(Box<Selection>, Box<Selection>),
+    Const(String),
+}
+
+impl Op {
+    fn local_schema<'a>(&self) -> LocalSchema {
+        match self {
+            Op::ScanOp(op) => op.ls.clone(),
+            Op::FilterOp(op) => op.ls.clone(),
+            Op::JoinOp(op) => op.ls.clone(),
+        }
+    }
+}
+
+impl LocalSchema {
+    fn get_field_idx(&self, colref: &ColRef) -> u32 {
+        for i in 0..self.columns.len() {
+            if colref == &self.columns[i] {
+                return i as u32;
+            }
+        }
+
+        panic!(
+            "Failed to find column reference {:?} in local schema.",
+            colref
+        )
+    }
+
+    fn from_meta_table(meta_schema: &MetaTableDef, table_alias: &str) -> LocalSchema {
+        let mut columns = Vec::new();
+
+        for column in &meta_schema.schema.columns {
+            columns.push(ColRef {
+                table: table_alias.to_string(),
+                column: column.0.clone(),
+            })
+        }
+
+        LocalSchema { columns }
+    }
+
+    fn cat(schema1: &LocalSchema, schema2: &LocalSchema) -> LocalSchema {
+        let mut columns = schema1.columns.clone();
+        columns.append(&mut schema2.columns.clone());
+
+        LocalSchema { columns }
+    }
 }
 
 impl Metadata {
@@ -130,6 +195,40 @@ impl Selection {
             _ => Option::None,
         }
     }
+
+    fn apply_filter_ops(&self, mut op: Op) -> Op {
+        match self {
+            Selection::Identity(_) => panic!("Identity filter not supported"),
+            Selection::Const(_) => panic!("Const filter not supported"),
+            Selection::And(subfilters) => {
+                for subfilter in subfilters {
+                    op = subfilter.apply_filter_ops(op);
+                }
+                op
+            }
+            Selection::Eq(l, r) => {
+                if let Selection::Const(_rep) = l.as_ref() {
+                    unimplemented!()
+                } else if let Selection::Const(rep) = r.as_ref() {
+                    if let Selection::Identity(colref) = l.as_ref() {
+                        let ls = op.local_schema();
+                        Op::FilterOp(Box::new(OpFilter {
+                            r#type: "filter".to_string(),
+                            field: op.local_schema().get_field_idx(colref),
+                            input: op,
+                            op: "==".to_string(),
+                            value: rep.to_string(),
+                            ls,
+                        }))
+                    } else {
+                        panic!("Can't match dual-value filter")
+                    }
+                } else {
+                    panic!("Can't match dual-ref filter")
+                }
+            }
+        }
+    }
 }
 
 impl From<&sqlparser::ast::Expr> for Selection {
@@ -147,7 +246,11 @@ impl From<&sqlparser::ast::Expr> for Selection {
             },
             Expr::CompoundIdentifier(idents) => Selection::Identity(idents.as_slice().into()),
             Expr::Nested(subexpr) => subexpr.as_ref().into(),
-            _ => panic!("Unsupported selection type"),
+            Expr::Value(v) => match v {
+                Value::Number(val_str, _) => Selection::Const(val_str.to_string()),
+                _ => panic!("Unsupported value type"),
+            },
+            _ => panic!("Unsupported selection type: {:?}", expr),
         }
     }
 }
@@ -188,7 +291,7 @@ fn plan(query: &Query, meta: &Metadata) -> Option<Op> {
     let mut root_op = Option::<Op>::None;
 
     let mut scans = Vec::<OpScan>::new();
-    for (_alias, table) in table_namespace.iter() {
+    for (alias, table) in table_namespace.iter() {
         let table_meta = meta
             .tables
             .iter()
@@ -202,12 +305,17 @@ fn plan(query: &Query, meta: &Metadata) -> Option<Op> {
             file: table_meta.file.to_string(),
             filetype: table_meta.filetype.to_string(),
             schema: table_meta.schema.columns.iter().map(|c| c.1).collect(),
+            ls: LocalSchema::from_meta_table(table_meta, alias),
         };
 
         scans.push(scan);
     }
 
-    let selection: Selection = select.selection.as_ref().unwrap().into();
+    let selection: Selection = if let Some(filter) = &select.selection {
+        filter.into()
+    } else {
+        Selection::And(vec![])
+    };
 
     fn _find_scan(scans: &mut Vec<OpScan>, table: &str) -> Option<OpScan> {
         for i in 0..scans.len() {
@@ -218,22 +326,34 @@ fn plan(query: &Query, meta: &Metadata) -> Option<Op> {
         Option::None
     }
 
-    if let Some((ltable_ref, rtable_ref)) = selection.as_equijoin() {
-        let ltable_ref = ltable_ref.resolve_aliases(&table_namespace);
-        let rtable_ref = rtable_ref.resolve_aliases(&table_namespace);
+    if scans.len() == 1 {
+        // No join
+        root_op = Some(Op::ScanOp(Box::new(scans.pop().unwrap())));
+    } else {
+        // With join; a shitty dual-table optimizer lives here
+        if let Some((ltable_ref, rtable_ref)) = selection.as_equijoin() {
+            let ltable_ref = ltable_ref.resolve_aliases(&table_namespace);
+            let rtable_ref = rtable_ref.resolve_aliases(&table_namespace);
 
-        let ltable = _find_scan(&mut scans, &ltable_ref.table).unwrap();
-        let rtable = _find_scan(&mut scans, &rtable_ref.table).unwrap();
+            let ltable = _find_scan(&mut scans, &ltable_ref.table).unwrap();
+            let rtable = _find_scan(&mut scans, &rtable_ref.table).unwrap();
 
-        let join = Op::JoinOp(Box::new(OpJoin {
-            r#type: "hashjoin".to_string(),
-            probe: Op::ScanOp(Box::new(ltable)),
-            probe_join_attribute: meta.attribute_index(&ltable_ref),
-            build: Op::ScanOp(Box::new(rtable)),
-            build_join_attribute: meta.attribute_index(&rtable_ref),
-        }));
+            let ls = LocalSchema::cat(&ltable.ls, &rtable.ls);
+            let join = Op::JoinOp(Box::new(OpJoin {
+                r#type: "hashjoin".to_string(),
+                probe: Op::ScanOp(Box::new(ltable)),
+                probe_join_attribute: meta.attribute_index(&ltable_ref),
+                build: Op::ScanOp(Box::new(rtable)),
+                build_join_attribute: meta.attribute_index(&rtable_ref),
+                ls,
+            }));
 
-        root_op = Some(join);
+            root_op = Some(join);
+        }
+    }
+
+    if let Some(op) = root_op {
+        root_op = Some(selection.apply_filter_ops(op));
     }
 
     root_op
@@ -248,11 +368,7 @@ fn unwrap_table_name(parts: &[Ident]) -> String {
 }
 
 fn main() {
-    let sql = "SELECT ZIP, SUM(PRICE-COST)
-    FROM LINEITEM L, PART P, ORDERS O
-    WHERE L.PKEY=P.PKEY
-    GROUP BY ZIP
-    ORDER BY ZIP ASC;";
+    let sql = "SELECT COST FROM PART P WHERE P.PKEY = 5;";
 
     let meta = Metadata {
         path: "drivers/sample_queries/data/".to_string(),
@@ -283,7 +399,7 @@ fn main() {
             },
             MetaTableDef {
                 name: "PART".to_string(),
-                file: "part.tbl.bz2".to_string(),
+                file: "part.tbl".to_string(),
                 filetype: "text".to_string(),
                 schema: MetaSchema {
                     columns: vec![
@@ -295,9 +411,10 @@ fn main() {
         ],
     };
 
-    let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
+    let dialect = GenericDialect {};
 
     let ast = Parser::parse_sql(&dialect, sql).unwrap().remove(0);
+    // println!("{:#?}", ast);
 
     let query: Box<Query> = match ast {
         Statement::Query(q) => q,
@@ -305,6 +422,7 @@ fn main() {
     };
     let exec_plan = plan(&query, &meta).unwrap();
 
+    // println!("{:#?}", exec_plan);
     println!("{}", plan_to_json(&exec_plan, &meta));
 }
 
@@ -367,11 +485,32 @@ impl OpJoin {
     }
 }
 
+impl OpFilter {
+    fn preflight(&self, global: &mut object::Object) {
+        self.input.preflight(global);
+
+        global["filterX"] = object! {
+            type: "filter",
+            op: self.op.clone(),
+            field: self.field,
+            value: self.value.clone(),
+        };
+    }
+
+    fn node(&self) -> JsonValue {
+        object! {
+            name: "filterX",
+            input: self.input.node(),
+        }
+    }
+}
+
 impl Op {
     fn preflight(&self, global: &mut object::Object) {
         match self {
             Op::ScanOp(op) => op.preflight(global),
             Op::JoinOp(op) => op.preflight(global),
+            Op::FilterOp(op) => op.preflight(global),
         }
     }
 
@@ -379,6 +518,7 @@ impl Op {
         match self {
             Op::ScanOp(op) => op.node(),
             Op::JoinOp(op) => op.node(),
+            Op::FilterOp(op) => op.node(),
         }
     }
 }
