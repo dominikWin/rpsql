7e8e7e4325147e8564b70cc2e1cc8ad424a40485
Dominik Winecki <dominikwinecki@gmail.com>
2021-04-21 13:58:48 -0400

    Add standard selection ops

--- src/planner.rs
+++ src/planner.rs
@@ -153,6 +153,11 @@ enum Selection {
     Identity(ColRef),
     And(Vec<Selection>),
     Eq(Box<Selection>, Box<Selection>),
+    NotEq(Box<Selection>, Box<Selection>),
+    Lt(Box<Selection>, Box<Selection>),
+    Gt(Box<Selection>, Box<Selection>),
+    LtEq(Box<Selection>, Box<Selection>),
+    GtEq(Box<Selection>, Box<Selection>),
     Const(String),
 }
 
@@ -200,6 +205,18 @@ impl Selection {
     }
 
     fn apply_filter_ops(&self, mut op: Op, existing_equalities: &EqualityFactSet) -> Op {
+        fn _compose_binary_filter(bin_op: &str, op: Op, colref: &ColRef, rep: &str) -> Op {
+            let vs = op.virtual_schema();
+            Op::FilterOp(Box::new(OpFilter {
+                field: op.virtual_schema().get_field_idx(colref),
+                input: op,
+                op: bin_op.to_string(),
+                value: rep.to_string(),
+                vs,
+                cfg_name: Option::None,
+            }))
+        }
+
         match self {
             Selection::Identity(_) => panic!("Identity filter not supported"),
             Selection::Const(_) => panic!("Const filter not supported"),
@@ -214,15 +231,7 @@ impl Selection {
                     (Selection::Const(_), Selection::Const(_)) => unimplemented!(),
                     (Selection::Identity(colref), Selection::Const(rep))
                     | (Selection::Const(rep), Selection::Identity(colref)) => {
-                        let vs = op.virtual_schema();
-                        Op::FilterOp(Box::new(OpFilter {
-                            field: op.virtual_schema().get_field_idx(colref),
-                            input: op,
-                            op: "==".to_string(),
-                            value: rep.to_string(),
-                            vs,
-                            cfg_name: Option::None,
-                        }))
+                        _compose_binary_filter("==", op, colref, rep)
                     }
                     (Selection::Identity(colref1), Selection::Identity(colref2)) => {
                         if existing_equalities.are_equal(colref1, colref2) {
@@ -235,6 +244,49 @@ impl Selection {
                     _ => panic!("Unknown equality configuration."),
                 }
             }
+            Selection::NotEq(l, r) => match (l.as_ref(), r.as_ref()) {
+                (Selection::Identity(colref), Selection::Const(rep))
+                | (Selection::Const(rep), Selection::Identity(colref)) => {
+                    _compose_binary_filter("!=", op, colref, rep)
+                }
+                _ => panic!("Unknown inequality configuration."),
+            },
+            Selection::Lt(l, r) => match (l.as_ref(), r.as_ref()) {
+                (Selection::Identity(colref), Selection::Const(rep)) => {
+                    _compose_binary_filter("<", op, colref, rep)
+                }
+                (Selection::Const(rep), Selection::Identity(colref)) => {
+                    _compose_binary_filter(">", op, colref, rep)
+                }
+                _ => panic!("Unknown less-than configuration."),
+            },
+            Selection::Gt(l, r) => match (l.as_ref(), r.as_ref()) {
+                (Selection::Identity(colref), Selection::Const(rep)) => {
+                    _compose_binary_filter(">", op, colref, rep)
+                }
+                (Selection::Const(rep), Selection::Identity(colref)) => {
+                    _compose_binary_filter("<", op, colref, rep)
+                }
+                _ => panic!("Unknown greater-than configuration."),
+            },
+            Selection::LtEq(l, r) => match (l.as_ref(), r.as_ref()) {
+                (Selection::Identity(colref), Selection::Const(rep)) => {
+                    _compose_binary_filter("<=", op, colref, rep)
+                }
+                (Selection::Const(rep), Selection::Identity(colref)) => {
+                    _compose_binary_filter(">=", op, colref, rep)
+                }
+                _ => panic!("Unknown less-than-equal configuration."),
+            },
+            Selection::GtEq(l, r) => match (l.as_ref(), r.as_ref()) {
+                (Selection::Identity(colref), Selection::Const(rep)) => {
+                    _compose_binary_filter(">=", op, colref, rep)
+                }
+                (Selection::Const(rep), Selection::Identity(colref)) => {
+                    _compose_binary_filter("<=", op, colref, rep)
+                }
+                _ => panic!("Unknown greater-than-equal configuration."),
+            },
         }
     }
 }
@@ -247,6 +299,26 @@ impl From<&sqlparser::ast::Expr> for Selection {
                     Selection::And(vec![left.as_ref().into(), right.as_ref().into()])
                 }
                 BinaryOperator::Eq => Selection::Eq(
+                    Box::new(left.as_ref().into()),
+                    Box::new(right.as_ref().into()),
+                ),
+                BinaryOperator::NotEq => Selection::NotEq(
+                    Box::new(left.as_ref().into()),
+                    Box::new(right.as_ref().into()),
+                ),
+                BinaryOperator::Lt => Selection::Lt(
+                    Box::new(left.as_ref().into()),
+                    Box::new(right.as_ref().into()),
+                ),
+                BinaryOperator::Gt => Selection::Gt(
+                    Box::new(left.as_ref().into()),
+                    Box::new(right.as_ref().into()),
+                ),
+                BinaryOperator::LtEq => Selection::LtEq(
+                    Box::new(left.as_ref().into()),
+                    Box::new(right.as_ref().into()),
+                ),
+                BinaryOperator::GtEq => Selection::GtEq(
                     Box::new(left.as_ref().into()),
                     Box::new(right.as_ref().into()),
                 ),
